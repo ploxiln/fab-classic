a295de48c36b443e96a66c7cc224567f9833498b
Pierce Lopez <pierce.lopez@gmail.com>
2020-03-20 01:31:51 -0400

    do not restore sys.path after importing fabfile
    leave it for parallel mode to be able to re-import tasks

--- fabric/main.py
+++ fabric/main.py
@@ -157,31 +157,21 @@ def load_fabfile(path, importer=None):
     # Get directory and fabfile name
     directory, fabfile = os.path.split(path)
     # If the directory isn't in the PYTHONPATH, add it so our import will work
-    added_to_path = False
-    index = None
     if directory not in sys.path:
         sys.path.insert(0, directory)
-        added_to_path = True
     # If the directory IS in the PYTHONPATH, move it to the front temporarily,
     # otherwise other fabfiles -- like Fabric's own -- may scoop the intended
     # one.
     else:
         i = sys.path.index(directory)
         if i != 0:
-            # Store index for later restoration
-            index = i
             # Add to front, then remove from original position
             sys.path.insert(0, directory)
             del sys.path[i + 1]
     # Perform the import (trimming off the .py)
     imported = importer(os.path.splitext(fabfile)[0])
-    # Remove directory from path if we added it ourselves (just to be neat)
-    if added_to_path:
-        del sys.path[0]
-    # Put back in original index if we moved it
-    if index is not None:
-        sys.path.insert(index + 1, directory)
-        del sys.path[0]
+
+    # leave sys.path changed so that parallel mode can re-import tasks
 
     # Actually load tasks
     docstring, new_style, classic, default = load_tasks_from_module(imported)
