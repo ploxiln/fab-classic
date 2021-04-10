bf79dccbdd8a252963f495e081e2d82c5c2389ef
Tim Snyder <snyder.tim@gmail.com>
2021-04-10 14:57:37 -0400

    fix error() output for local() with capture=True
    error() would assume that stdout/stderr were already printed
    if they were not hidden, but that is not true for
    local(... capture=True), so make sure error() includes
    stderr/stdout in this special case

--- fabric/operations.py
+++ fabric/operations.py
@@ -1241,7 +1241,10 @@ def local(command, capture=False, shell=None, pty=True, encoding='utf-8'):
     if p.returncode not in env.ok_ret_codes:
         out.failed = True
         msg = "local() encountered an error (return code %s) while executing '%s'" % (p.returncode, command)
-        error(message=msg, stdout=out, stderr=err)
+        # error() assumes the caller has already printed stdout/stderr if they are not hidden,
+        # but that is not true for local() with capture=True, so fudge it so error() includes them
+        with settings(hide('stdout', 'stderr') if capture else None):
+            error(message=msg, stdout=out, stderr=err)
     out.succeeded = not out.failed
     # If we were capturing, this will be a string; otherwise it will be None.
     return out
